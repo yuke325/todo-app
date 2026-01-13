45a6c9777459fbfb24b2a2e6920c12560f80f838
yuke325 <yusukesaikyou931@gmail.com>
2026-01-13 11:39:15 +0900

    build(deps)!: upgrade ruby and rails version
    Recreate Gemfile.lock to resolve fresh dependencies.
    
    BREAKING CHANGE: upgrade ruby and rails versions

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_01_12_174119) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_13_022931) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title", null: false
     t.boolean "completed", default: false, null: false
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
+    t.string "title", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
     t.datetime "created_at", null: false
+    t.string "email", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
